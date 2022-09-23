a7d6c1328281fbb7f3921ac9cfea5201f1fa10d5
Anton Vashenko <vashchenko-96@mail.ru>
2022-09-23 09:34:29 +0300

    Add fourth homework task

---
+++ Homework/Lesson_01/01.04.py
@@ -0,0 +1,16 @@
+# Напишите программу, которая по заданному номеру четверти,
+# показывает диапазон возможных координат точек в этой четверти (x и y).
+
+
+quarter = int(input("Enter number of quarter: "))
+
+if quarter == 1:
+    print("x > 0, y > 0")
+elif quarter == 2:
+    print("x < 0, y > 0")
+elif quarter == 3:
+    print("x < 0, y < 0")
+elif quarter == 4:
+    print("x > 0, y < 0")
+else:
+    print("The quarter was entered incorrectly!")
